2cc046036b31ea261b1f2aa223dd4475b8982a58
RamyaShreeH <ramya.shree.2k12@gmail.com>
2023-11-06 12:52:31 +0530

    changed url in NotAttempted page

--- src/pages/NotAttempted.js
+++ src/pages/NotAttempted.js
@@ -3,10 +3,10 @@ import '../App.css';
 const NotAttempted = () =>{
     const navigate = useNavigate();
     return(
-        <div className="App-header">
+        <div className="quiz-page">
             <h1 className="question">Time's Up! </h1>
             <h1 className="question">Oops! quiz not completed 🥺.</h1>
-            <button className="btn btn-primary mt-2" onClick={()=>navigate("/")}>Back to home</button>
+            <button className="btn btn-primary mt-2" onClick={()=>navigate("/Gk-quiz23/")}>Back to home</button>
         </div>
     )
 }
